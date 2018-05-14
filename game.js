dbbfc16d933dfe12f498f3c07707e2f1ebe66c25
manuelottlik <manuel.ottlik@web.de>
2018-05-14 11:28:49 +0200

    changes

--- game.js
+++ game.js
@@ -1,20 +1,28 @@
 //Objektvariablen
 var agCar;
 var agTrail;
+var agGoal;
 
 //Variablen für Abmessungen etc.
-var canvasSize = 1000;
+var canvasSize = 600;
 var carSize = canvasSize/15;
 var trailSize = canvasSize/25;
+var goalX = -4;
+var goalY = 4;
 
 //diese p5.js-Standardfunktion wird zum Start einmal ausgeführt
 function setup() {
     //die Leinwand wird erzeugt
     createCanvas(canvasSize, canvasSize);
 
+
+    //Elemente sollen ohne Rahmen gezeichnet werden
+    noStroke();
+
     //Objekt des Autos wird erzeugt
     agCar = new AgCar();
     agTrail = [];
+    agGoal = new AgGoal();
 }
 
 //diese p5.js-Standardfunktion wird Frame für Frame erneut ausgeführt
@@ -22,47 +30,76 @@ function draw() {
     //Hintergrund
     background(0,136,255);
 
-    //Auto wird angezeigt
-    agCar.show();
-
     //durch alle Spurelemente iterieren
     for(var i = agTrail.length - 1; i >= 0; i--) {
         //Spur anzeigen
         agTrail[i].show();
     }
+
+    //Ziel wird angezeigt
+    agGoal.show();
+
+    //Auto wird angezeigt
+    agCar.show();
 }
 
 function AgCar() {
-    this.x = carSize;
-    this.y = carSize;
+    this.x = canvasSize/2;
+    this.y = canvasSize/2;
+    this.direction = 0;
     this.trail = [];
 
     this.show = function () {
-        ellipse(this.x, this.y, carSize, carSize);
+        fill(0,0,255);
+        translate(this.x, this.y);
+        rotate(this.direction);
+        triangle(-carSize/2, -carSize/2 + 5, -carSize/2, carSize/2 - 5, carSize/2,0);
+        rotate(-this.direction);
+        translate(-this.x, -this.y);
     }
 
-    this.moveUp = function () {
-        this.trackMovement();
-        this.y = this.y - carSize;
+    this.turnLeft = function() {
+        this.direction -= PI/2;
     }
 
-    this.moveDown = function () {
-        this.trackMovement();
-        this.y = this.y + carSize;
+    this.turnRight = function() {
+        this.direction += PI/2;
     }
 
-    this.moveLeft = function () {
-        this.trackMovement();
-        this.x = this.x - carSize;
-    }
-
-    this.moveRight = function () {
-        this.trackMovement();
-        this.x = this.x + carSize;
-    }
-
-    this.trackMovement = function () {
+    this.moveForward = function () {
         agTrail.push(new AgTrail(this.x, this.y));
+
+        if(cos(this.direction) == 1) {
+            this.x += carSize;
+        }
+        if(cos(this.direction) == -1) {
+            this.x -= carSize;
+        }
+
+        if(sin(this.direction) == 1) {
+            this.y += carSize;
+        }
+        if(sin(this.direction) == -1) {
+            this.y -= carSize;
+        }
+
+        if(this.x - canvasSize/2 - agGoal.x == 0 && this.y - canvasSize/2 - agGoal.y == 0) {
+            alert('YOU WON\n steps: '+agTrail.length);
+        }
+    }
+}
+
+function AgGoal() {
+    this.x = carSize * round(random(-7.5, 7.5));
+    this.x = carSize * goalX;
+    this.y = carSize * round(random(-7.5, 7.5));
+    this.y = carSize * goalY;
+
+    this.show = function () {
+        fill(255,255,0);
+        translate(canvasSize/2, canvasSize/2);
+        ellipse(this.x, this.y, carSize);
+        translate(-canvasSize/2, -canvasSize/2);
     }
 }
 
@@ -71,24 +108,21 @@ function AgTrail(x,y) {
     this.y = y;
 
     this.show = function() {
+        fill(0,0,255);
         ellipse(this.x, this.y, trailSize, trailSize);
     }
 }
 
 function keyPressed() {
     if (keyCode == UP_ARROW) {
-        agCar.moveUp();
-    }
-
-    if (keyCode == DOWN_ARROW) {
-        agCar.moveDown();
+        agCar.moveForward();
     }
 
     if (keyCode == LEFT_ARROW) {
-        agCar.moveLeft();
+        agCar.turnLeft();
     }
 
     if (keyCode == RIGHT_ARROW) {
-        agCar.moveRight();
+        agCar.turnRight();
     }
 }
